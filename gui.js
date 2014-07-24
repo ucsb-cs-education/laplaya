d31e4b2b0df6b95b5ac8e7f945a5d99a13e73485
loganortega <loganportega@gmail.com>
2014-07-24 11:26:50 -0700

    corrected palette deletion properties

--- gui.js
+++ gui.js
@@ -1316,6 +1316,22 @@ IDE_Morph.prototype.createPalette = function () {
         if (droppedMorph instanceof SpriteMorph) {
             return false;
         }
+        else if (droppedMorph instanceof BlockMorph) {
+            return true;
+        }
+        else if (droppedMorph instanceof DialogBoxMorph) {
+            return true;
+        }
+        else if (droppedMorph instanceof SpriteIconMorph) {
+            return true;
+        }
+        else if (droppedMorph instanceof CostumeIconMorph) {
+            return true;
+        }
+        else if (droppedMorph instanceof CommentMorph) {
+            return true;
+        }
+
     }
 
     this.palette.reactToDropOf = function (droppedMorph) {
@@ -1339,7 +1355,7 @@ IDE_Morph.prototype.createPalette = function () {
                 droppedMorph.slideBackTo(myself.world().hand.grabOrigin);
             }
             else {
-               myself.removeSprite(droppedMorph);
+               droppedMorph.destroy();
             }
         }
         else if (droppedMorph instanceof CommentMorph) {
