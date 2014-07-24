a8e4bded33fdb871171b7aba32642fce473acd43
rtwaltman <rtwaltman@umail.ucsb.edu>
2014-07-24 13:15:07 -0700

    Minor Updates - Removed URL changing based on sharing files - Updated snap.html to default into developer mode and remove a test file auto-load that was accidentally pushed

--- octopi_cloud.js
+++ octopi_cloud.js
@@ -121,14 +121,6 @@ Cloud.prototype.rawOpenProject = function (proj, ide, callback) {
                 ide.setProjectId(null);
                 ide.hasChangedMedia = true;
             }
-// It might be useful to alter the URL like this for public saves, so that it is easier to link.... but
-            if (response.public === true) {
-                location.hash = //'#octopi-cloud:' +
-                    encodeURIComponent(proj.file_id);
-            } else
-            {
-                location.hash = '';
-            }
             callback();
         },
         ide.cloudError(),
