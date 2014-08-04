9376f201fc36adaf368924255012f1c6af89e5dc
Timothy Martinez <tmartinez@umail.ucsb.edu>
2014-08-04 11:48:58 -0700

    Add Feedback Option, resize instructions div
    -ToDo: Figure out what to do with feedback (send to server, store in
    laplaya file?)

--- gui.js
+++ gui.js
@@ -2984,8 +2984,8 @@ IDE_Morph.prototype.createInstructions = function (x, y) {
 	    instructionsDiv.style.position = "absolute";
 	    instructionsDiv.style.left = x + "px";
 	    instructionsDiv.style.top = y + "px";
-	    instructionsDiv.style.width = "25%";
-	    instructionsDiv.style.height = "25%";
+	    instructionsDiv.style.width = "420px";
+	    instructionsDiv.style.height = "300px";
 	    instructionsDiv.style.zIndex = "2";
 	    instructionsDiv.style.backgroundColor = '#FFFFFF';
 	    instructionsDiv.style.padding = '10px';
@@ -3528,18 +3528,31 @@ IDE_Morph.prototype.saveTask = function () {
         console.log(project);
         if (myself.analysisProcessor) {
             var results = myself.analysisProcessor(project);
-            makePop(results);
+            makePop(results['html']);
         }
         else if (myself.developer == true) {
-            results = '<b>Here for testing purposes</b><img src="Costumes/cat3.png">';
             makePop(results);
         }
+        myself.results = results;
     }
     octopi_xml2js(xml, callback);
 };
 
 document.documentElement.style.overflow = "hidden";
 function makePop(str) {
+    var feedbackForm =
+        '<p><b>Was this helpful?</b></p>' +
+        '<form>' +
+        '<input type="radio" name="response" value="yes">Yes</input>' +
+        '<input type="radio" name="response" value="no">No</input>' +
+        '<input type="radio" name="response" value="maybe">Mabye</input>'+
+        '<p>Any specific feeback?<br><textarea name="feedback"></textarea></br>'+
+        '<br><input type="submit" value="Submit Feedback" action="?"></br></p>'+
+        '</form>';
+    var closeButton =
+        '<div style ="position:absolute; right:40px">'+
+        '<button style="position: fixed;" onclick="hideDiv(results)">&#10006</button>'+
+        '</div>';
     var checkDiv = document.getElementById('results');
     if (checkDiv == null) {
         var div = document.createElement('div');
@@ -3553,11 +3566,12 @@ function makePop(str) {
         div.style.width = "75%";
         div.style.height = "75%";
         div.style.overflow = "scroll";
-        div.innerHTML = str;
-        div.onclick = function(){
+        div.style.paddingLeft = "10px";
+        div.innerHTML = closeButton+ (str || '') + feedbackForm;
+        /*div.onclick = function(){
             div.style.visibility = "hidden";
             div.style.overflow = 'hidden';
-        }
+        }*/
         div.oncontextmenu = function () {
             return false;
         }
@@ -3565,16 +3579,21 @@ function makePop(str) {
     }
     else {
         if (checkDiv.style.visibility == "visible") {
-            checkDiv.innerHTML = str;
+            checkDiv.innerHTML = closeButton + (str || '') + feedbackForm;
         }
         else {
             checkDiv.style.visibility = "visible";
             checkDiv.style.overflow = 'scroll';
-            checkDiv.innerHTML = str; 
+            checkDiv.innerHTML = closeButton + (str || '') + feedbackForm;
         }
     }
 }
 
+function hideDiv(div) {
+    //var div = document.getElementById(divName);
+    div.style.visibility = 'hidden';
+    div.style.overflow = 'hidden';
+}
 // IDE_Morph sprite list access
 
 IDE_Morph.prototype.addNewSprite = function (name) {
@@ -5212,9 +5231,11 @@ IDE_Morph.prototype.toggleStageSize = function (isSmall) {
     if (instructionsDiv != null) {
         if (this.isSmallStage) {
             instructionsDiv.style.width = "10%";
+            instructionsDiv.style.height = "400px";
         }
         else {
-            instructionsDiv.style.width = "25%";
+            instructionsDiv.style.width = "420px";
+            instructionsDiv.style.height = "300px";
         }
     }
     myself.createCorralBar();
