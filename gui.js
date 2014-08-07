c25806ea472febba6a1791ec423e87698623ce1d
rtwaltman <rtwaltman@umail.ucsb.edu>
2014-08-07 15:01:58 -0700

    Fixed Right Clicking Categories Bug

--- gui.js
+++ gui.js
@@ -1243,7 +1243,7 @@ IDE_Morph.prototype.createCategories = function () {
                 inPalette = StageMorph.prototype.inPaletteBlocks;
             return Object.keys(inPalette).some(function (any) {
 
-                var temp = (defs[any] || more[category][any]) && (((inPalette[any] == false) && defs[any] &&
+                var temp = defs[any] && (((inPalette[any] == false) && defs[any] &&
                 	(defs[any].category === category ||
                    	contains((more[category] || []), any))) || (inPalette[any] == false
                    	&& category == 'variables' && any.indexOf('reportGetVar') > -1));
