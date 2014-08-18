bb1508c45b779d4cdd251961f5d66bad69ad718b
loganortega <loganportega@gmail.com>
2014-08-18 16:01:44 -0700

    remove turtle from costume drop-down list

--- blocks.js
+++ blocks.js
@@ -7386,7 +7386,7 @@ InputSlotMorph.prototype.costumesMenu = function () {
         dict,
         allNames = [];
     if (rcvr instanceof SpriteMorph) {
-        dict = {Turtle: ['Turtle']};
+        dict = {Empty: ['Empty']};
     } else { // stage
         dict = {Empty: ['Empty']};
     }
