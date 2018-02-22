596243f5e10211cb387769ef33429b7655581743
saputera <saputera@uchicago.edu>
2018-02-22 10:36:41 -0600

    Trying to add Add/Remove block from palette to custom blocks in dev view

--- byob.js
+++ byob.js
@@ -723,6 +723,23 @@ CustomCommandBlockMorph.prototype.userMenu = function () {
               }
           );
       }
+      else if (!this.inPalette) {
+          menu.addItem(
+              "Add to block palette",
+              function () {
+                  this.switchInPalette(true);
+                  if (!(StageMorph.prototype.inPaletteBlocks['cat-' + this.category])) {
+                      var capString = this.category.charAt(0).toUpperCase() + this.category.slice(1),
+                          catButtonIndex = ide.categories.children.map(function (e) {
+                              return e.labelString;
+                          }).indexOf(capString),
+                          catButton = ide.categories.children[catButtonIndex];
+
+                      catButton.changeCategory(true, ide, false);
+                  }
+              }
+          );
+      }
     }
     return menu;
 };
