d43befdf99f70c2e7a6c31cbb22866a7f658cf2b
saputera <saputera@uchicago.edu>
2018-02-23 11:38:52 -0600

    Testing

--- threads.js
+++ threads.js
@@ -3348,7 +3348,7 @@ Process.prototype.jump = function (val) {
 
 
 var x = parseFloat(varFrame.find('x').vars['x']);
-x = parseFloat(x + parseFloat(200));
+x = parseFloat(x + 200);
 this.doSetVar('x',x);
 
 /*
