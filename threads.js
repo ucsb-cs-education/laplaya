f2023c579c1546220cb325786e7a59b5a40fea3e
saputera <saputera@uchicago.edu>
2018-02-23 10:29:39 -0600

    Testing

--- threads.js
+++ threads.js
@@ -3337,7 +3337,7 @@ Process.prototype.gridLeft = function () {
 Process.prototype.jump = function (val) {
   var rcvr = this.blockReceiver();
   var varFrame = this.context.variables;
-  var x = parseFloat(varFrame.find('x').vars['x']);
+  var x = parseFloat(varFrame.find('x').vars['x']) + parseFloat(100);
   this.doSetVar('x',x);
 
 /*
